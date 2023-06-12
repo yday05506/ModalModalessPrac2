1b66b82abe2050be93af9cebca1b9a5289734347
yday05506 <yangdayeon2@gmail.com>
2023-06-12 12:02:15 +0900

    메시지박스를 활용한 모달창 띄우기 및 버튼 입력 받기

--- ModalModalessPrac2/Form1.Designer.cs
+++ ModalModalessPrac2/Form1.Designer.cs
@@ -1,4 +1,7 @@
-﻿namespace ModalModalessPrac2
+﻿using System;
+using System.Windows.Forms;
+
+namespace ModalModalessPrac2
 {
     partial class Form1
     {
@@ -35,6 +38,21 @@
         }
 
         #endregion
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("내용");
+            MessageBox.Show("내용", "제목");
+            
+            DialogResult result;
+            do
+            {
+                result = MessageBox.Show("내용", "제목", MessageBoxButtons.RetryCancel);
+            }while(result == DialogResult.Retry);
+            if(result == DialogResult.Cancel)
+            {
+                MessageBox.Show("사용자가 취소하였습니다.", "알림", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+        }
     }
 }
 
