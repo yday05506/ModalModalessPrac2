3697dc9bb7d963408156e5cb965848737b1b888e
yday05506 <yangdayeon2@gmail.com>
2023-06-12 12:17:08 +0900

    MDI 인터페이스 맛 보기

--- ModalModalessPrac2/Form1.Designer.cs
+++ ModalModalessPrac2/Form1.Designer.cs
@@ -12,23 +12,11 @@ namespace ModalModalessPrac2
                 Text = "제목 글자";
             }
         }
-
-        /// <summary>
-        /// 필수 디자이너 변수입니다.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
-
-        /// <summary>
-        /// 사용 중인 모든 리소스를 정리합니다.
-        /// </summary>
-        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
-        protected override void Dispose(bool disposing)
+        
+        public Form1()
         {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
+            InitializeComponent();
+            IsMdiContainer = true;
         }
 
         #region Windows Form 디자이너에서 생성한 코드
@@ -65,6 +53,7 @@ namespace ModalModalessPrac2
         private void Button2_Click(object sender, EventArgs e)
         {
             CustomForm form = new CustomForm();
+            form.MdiParent = this;
             form.Show();
         }
     }
